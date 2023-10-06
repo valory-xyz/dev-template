376e7c3f006194bf37b5896904ea072020d27e42
David Vilela <dvilelaf@gmail.com>
2023-10-06 17:45:54 +0200

    fix: do not print openai key

--- packages/algovera/connections/chat_completion/connection.py
+++ packages/algovera/connections/chat_completion/connection.py
@@ -53,7 +53,7 @@ Standalone question:
 
 # system_prompt
 system_prompt = """
-Use the following pieces of context to answer the question at the end. 
+Use the following pieces of context to answer the question at the end.
 Additionally, you are also given the previous conversation history to help you answer the question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 """
@@ -196,7 +196,6 @@ class ChatCompletionConnection(BaseSyncConnection):
         }
         openai.api_key = self.openai_settings["openai_api_key"]
         self.dialogues = ChatCompletionDialogues(connection_id=PUBLIC_ID)
-        self.logger.info("OpenAI API Key: {}".format(openai.api_key))
 
     def main(self) -> None:
         """
