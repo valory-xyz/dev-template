a5545ca4f60a96b491f9c40ac69140c2fa489a66
David Vilela <dvilelaf@gmail.com>
2023-10-09 14:26:37 +0200

    fix: hashes

--- packages/algovera/skills/valory_chat_chained_abci/__init__.py
+++ packages/algovera/skills/valory_chat_chained_abci/__init__.py
@@ -22,4 +22,4 @@
 from aea.configurations.base import PublicId
 
 
-PUBLIC_ID = PublicId.from_str("valory/valory_chat_chained_abci:0.1.0")
+PUBLIC_ID = PublicId.from_str("algovera/valory_chat_chained_abci:0.1.0")
